de85da5e4dd41a30d89683e8e3e4e8b2f0fb4977
guillaume <gpitot@nine.com.au>
2020-08-31 15:47:26 +1000

    remove bad import

--- src/Theme/index.js
+++ src/Theme/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { createMuiTheme } from "@material-ui/core/styles";
 import { ThemeProvider } from "@material-ui/styles";
-import { red } from "@material-ui/core/colors";
 
 const theme = createMuiTheme({
   palette: {
