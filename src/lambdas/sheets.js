4e42fc0bc85219d4f88717c73bc595523444be19
guillaume <gpitot@nine.com.au>
2020-09-09 17:06:04 +1000

    labmda

--- src/lambdas/sheets.js
+++ src/lambdas/sheets.js
@@ -1,19 +1,20 @@
-const fetch = require("node-fetch");
+const { GoogleSpreadsheet } = require("google-spreadsheet");
 
 //AIzaSyBhPxjwHTAMUG-wAf1uF0lv965uVdJf4qA
 // https://sheets.googleapis.com/v4/spreadsheets/1SSnVv3nfogNozLj7Kw2xsP3KlENmzjeDhOLDZOWBl2k?key=AIzaSyBhPxjwHTAMUG-wAf1uF0lv965uVdJf4qA
 
-const RANKS =
-  "https://sheets.googleapis.com/v4/spreadsheets/1SSnVv3nfogNozLj7Kw2xsP3KlENmzjeDhOLDZOWBl2k";
-const HISTORY = "";
+const RANKS = "1SSnVv3nfogNozLj7Kw2xsP3KlENmzjeDhOLDZOWBl2k";
 const KEY = "AIzaSyBhPxjwHTAMUG-wAf1uF0lv965uVdJf4qA";
 
 exports.handler = async (event, context) => {
-  return fetch(`${RANKS}?key=${KEY}`)
-    .then((response) => response.json())
-    .then((data) => ({
-      statusCode: 200,
-      body: data,
-    }))
-    .catch((error) => ({ statusCode: 422, body: String(error) }));
+  const ranksDoc = new GoogleSpreadsheet(RANKS);
+  ranksDoc.useApiKey(KEY);
+  await ranksDoc.loadInfo();
+  const sheet = ranksDoc.sheetsByIndex[0];
+  const rows = await sheet.getRows(); // can pass in { limit, offset }
+  console.log(rows);
+  return {
+    status: 200,
+    body: "hello world",
+  };
 };
