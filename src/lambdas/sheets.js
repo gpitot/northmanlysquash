cec2cdbff40cbd4dec538b1b87372cfb3f1d39d8
guillaume <gpitot@nine.com.au>
2020-09-09 16:07:09 +1000

    lambda key

--- src/lambdas/sheets.js
+++ src/lambdas/sheets.js
@@ -1,12 +1,19 @@
-const fetch = require('node-fetch');
-const API_ENDPOINT = "https://icanhazdadjoke.com/";
+const fetch = require("node-fetch");
+
+//AIzaSyBhPxjwHTAMUG-wAf1uF0lv965uVdJf4qA
+// https://sheets.googleapis.com/v4/spreadsheets/1SSnVv3nfogNozLj7Kw2xsP3KlENmzjeDhOLDZOWBl2k?key=AIzaSyBhPxjwHTAMUG-wAf1uF0lv965uVdJf4qA
+
+const RANKS =
+  "https://sheets.googleapis.com/v4/spreadsheets/1SSnVv3nfogNozLj7Kw2xsP3KlENmzjeDhOLDZOWBl2k";
+const HISTORY = "";
+const KEY = "AIzaSyBhPxjwHTAMUG-wAf1uF0lv965uVdJf4qA";
 
 exports.handler = async (event, context) => {
-  return fetch(API_ENDPOINT, { headers: { "Accept": "application/json" } })
-    .then(response => response.json())
-    .then(data => ({
+  return fetch(`${RANKS}?key=${KEY}`)
+    .then((response) => response.json())
+    .then((data) => ({
       statusCode: 200,
-      body: data.joke
+      body: data,
     }))
-    .catch(error => ({ statusCode: 422, body: String(error) }));
+    .catch((error) => ({ statusCode: 422, body: String(error) }));
 };
