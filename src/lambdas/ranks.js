a426c63ceced5f809dfa2c847fe5bb74120e9c6c
guillaume <gpitot@nine.com.au>
2020-09-09 17:30:01 +1000

    lambda for ranks

--- src/lambdas/ranks.js
+++ src/lambdas/ranks.js
@@ -1,8 +1,5 @@
 const { GoogleSpreadsheet } = require("google-spreadsheet");
 
-//AIzaSyBhPxjwHTAMUG-wAf1uF0lv965uVdJf4qA
-// https://sheets.googleapis.com/v4/spreadsheets/1SSnVv3nfogNozLj7Kw2xsP3KlENmzjeDhOLDZOWBl2k?key=AIzaSyBhPxjwHTAMUG-wAf1uF0lv965uVdJf4qA
-
 const RANKS = "1SSnVv3nfogNozLj7Kw2xsP3KlENmzjeDhOLDZOWBl2k";
 const KEY = "AIzaSyBhPxjwHTAMUG-wAf1uF0lv965uVdJf4qA";
 
@@ -10,11 +7,18 @@ exports.handler = async (event, context) => {
   const ranksDoc = new GoogleSpreadsheet(RANKS);
   ranksDoc.useApiKey(KEY);
   await ranksDoc.loadInfo();
-  const sheet = ranksDoc.sheetsByIndex[0];
-  const rows = await sheet.getRows(); // can pass in { limit, offset }
-  console.log(rows);
+  const sheet = ranksDoc.sheetsById[2004016643];
+  const rows = await sheet.getRows({
+    limit: 40,
+  }); // can pass in { limit, offset }
+  const ranks = rows.map(({ Name, Points, Win, Loss }) => [
+    Name,
+    Points,
+    Win,
+    Loss,
+  ]);
   return {
     status: 200,
-    body: "hello world",
+    body: ranks,
   };
 };
