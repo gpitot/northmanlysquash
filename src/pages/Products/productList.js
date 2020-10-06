52f8c5f8fd99643b6c3758ea90868aa4e2c90ffb
guillaume <gpitot@nine.com.au>
2020-10-06 18:37:19 +1100

    weigjt and size

--- src/pages/Products/productList.js
+++ src/pages/Products/productList.js
@@ -1,10 +1,10 @@
 export default [
-  ["aggressor.webp", "Aggressor Squash Racquet ", 199],
-  ["assault.webp", "Assault Squash Racquet", 180],
-  ["hornet.webp", "Hornet Squash Racquet", 190],
-  ["stealth.webp", "Stealth Squash Racquet", 199],
-  ["theboss.webp", "The Boss Squash Racquet", 190],
-  
+  ["aggressor.webp", "Aggressor Squash Racquet ", 199, "490", "125gm"],
+  ["assault.webp", "Assault Squash Racquet", 180, "500", "125gm"],
+  ["hornet.webp", "Hornet Squash Racquet", 190, "480", "130gm"],
+  ["stealth.webp", "Stealth Squash Racquet", 199, "490", "125gm"],
+  ["theboss.webp", "The Boss Squash Racquet", 190, "490", "130gm"],
+
   ["RedbackShoe_large.webp", "Redback Shoes", 149],
   ["balls.webp", "Balls", 5],
   ["grips.webp", "Grips", 10],
